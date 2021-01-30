8352e0a97150ca9a244aa06932607880eb85bd82
Eric Omine <eric.omine@gmail.com>
2021-01-30 20:09:52 -0300

    (wip) Cleanup

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -69,8 +69,6 @@ class HomePage extends CubitPage<HomeCubit> {
   }
 
   Widget _buildShowList(BuildContext context, HomeState state) {
-    final width = MediaQuery.of(context).size.width;
-
     if (state.noResultFound) {
       return Center(
         child: Column(
@@ -111,12 +109,3 @@ class HomePage extends CubitPage<HomeCubit> {
         });
   }
 }
-
-final _tempTVShow = TVShow(
-    name: "Under the Dome",
-    imageMedium:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg",
-    imageOriginal:
-        "http://static.tvmaze.com/uploads/images/original_untouched/81/202627.jpg",
-    summary:
-        "<p><b>Under the Dome</b> is the story of a small town that is suddenly and inexplicably sealed off from the rest of the world by an enormous transparent dome. The town's inhabitants must deal with surviving the post-apocalyptic conditions while searching for answers about the dome, where it came from and if and when it will go away.</p>");
